857a79c5e853da84a8901e4020a5b9c0cf1ae0cb
Julian Battaglino <julianbattaglino@gmail.com>
2023-06-21 23:49:48 -0300

    aos global

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,28 @@
 import './styles.css';
 import "7.css/dist/7.css";
 import { Inter } from 'next/font/google';
+
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import React, { useEffect } from "react";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
- 
+
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
-    weight: ['300', '400', '500', '600', '700', '800', '900' ],
-    style: ['normal'],
-    subsets: ['latin'],
-  }); 
+  weight: ['300', '400', '500', '600', '700', '800', '900'],
+  style: ['normal'],
+  subsets: ['latin'],
+});
+
 export default function MyApp({ Component, pageProps }) {
+
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
+
   return (
     <main className={inter.className}>
       <Navbar />
